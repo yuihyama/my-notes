84cee113d68d17c187a8399934a5b3881a90f4f3
yuihyama <yuihyama@gmail.com>
2018-08-09 07:37:41 +0900

    Add file

---
+++ js/main.js
@@ -0,0 +1,14 @@
+jQuery.noConflict();
+
+jQuery(function() {
+  jQuery("h1").css("color", "darkcyan");
+});
+
+// or
+// jQuery(document).ready(function() {
+//   jQuery("h1").css("color", "darkcyan");
+// });
+
+jQuery(function() {
+  jQuery("#notes").css("color", "seagreen");
+});
